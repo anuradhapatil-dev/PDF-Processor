f0176837faade85272ed15e3c20a0a6ac5d38ce2
anuradha-dev <anuradhapatil.110@gmail.com>
2025-04-26 20:08:57 +0530

    Initial commit: Project setup with Spring Boot,Maven, and Java 22

---
+++ src/main/java/com/anuradha/pdfprocessor/pdf_processor/PdfProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.anuradha.pdfprocessor.pdf_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anuradha/pdfprocessor/pdf_processor/PdfProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anuradha.pdfprocessor.pdf_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
