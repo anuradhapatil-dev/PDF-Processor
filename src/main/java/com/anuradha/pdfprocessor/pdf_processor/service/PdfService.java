406f75ae9e11e4b6f0ef8b481fea796877d02e12
anuradhapatil-dev <anuradhapatil.110@gmail.com>
2025-04-28 18:52:57 +0530

    feat: add PdfService skeleton

---
+++ src/main/java/com/anuradha/pdfprocessor/pdf_processor/service/PdfService.java
@@ -0,0 +1,11 @@
+package com.anuradha.pdfprocessor.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PdfService {
+
+    public void mergePdfs() {
+        // TODO: Will implement merge logic here
+    }
+}
